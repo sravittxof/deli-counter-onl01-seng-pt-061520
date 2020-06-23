f26497c34714d947ea912101f738e0546f126160
Travis Fox <travis.fox90@gmail.com>
2020-06-23 01:24:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     puts "The line is currently:"
-    katz_deli.each_wih_index { |name, index| 
+    katz_deli.each_wih_index { |name, index| print ""
   end
 end
 
